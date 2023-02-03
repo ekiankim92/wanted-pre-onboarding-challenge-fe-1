12b771f16b72845d6e3b7080e644120cb9a6bfb4
ekim <eunkukkim1992@gmail.com>
2023-02-04 00:22:25 +0900

    [U]- useForm common update

--- src/components/common/form/authForm.tsx
+++ src/components/common/form/authForm.tsx
@@ -1,7 +1,21 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { schema } from "../../../utils/schema";
+
 export default function AuthForm() {
+  const { handleSubmit } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(schema),
+  });
+
+  const onClickSubmit = () => {
+    alert("testing");
+  };
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onClickSubmit)}>
       <div>This is Authform</div>
-    </>
+      <button>Click</button>
+    </form>
   );
 }
